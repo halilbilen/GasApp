ba98a3bb248cd9f5a9cdaa45b1dc12f7b725aa01
Halil Bilen <38647437+halilbilen@users.noreply.github.com>
2020-09-04 21:37:54 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Deneme</Text>
+      </View>
+    )
+  }
+}
+  
